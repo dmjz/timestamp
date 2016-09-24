6ef5f18dd2aa21c4159db9fc5ce221a3beace38c
David <djmzorn@gmail.com>
2016-09-24 20:08:23 +0000

    organize  server.js; add verified naturalToUnix conversion

--- server.js
+++ server.js
@@ -2,6 +2,59 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+// ----------------------------------------------------------------------------
+// Misc. helper functions and variables.
+var monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 
+        'July', 'August', 'September', 'October', 'November', 'December'];
+var daysInMonthArr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var daysInMonthLeapArr = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var secs_in_day = 24*60*60;
+var secs_in_year = 365*secs_in_day;
+var secs_in_leap_year = 366*secs_in_day;
+
+function getMonthNumber (monthString) {
+    return monthArr.indexOf(monthString);
+}
+
+function getDaysInMonth (monthCode) {
+    return daysInMonthArr[monthCode];
+}
+
+function isLeapYear (year) {
+    return year % 4 === 0;
+}
+
+// ----------------------------------------------------------------------------
+// Conversion functions: UNIX to natural date, and conversely.
+// Note: natural dates are given as arrays of strings: [day, month, year];
+function unixToNatural(unix) {
+    
+}
+
+function naturalToUnix(natural) {
+    var day = parseInt(natural[0]);
+    var month = getMonthNumber(natural[1]);
+    var year = parseInt(natural[2]);
+    var numLeapYears = Math.floor((year - 1970 + 1)/4);
+    var unix = (year - 1970 - numLeapYears)*secs_in_year
+                + numLeapYears*secs_in_leap_year;
+    var i = 0;
+    if (isLeapYear(year)) {
+        while (i < month) {
+            unix += daysInMonthLeapArr[i]*secs_in_day;
+            i++;
+        }
+    } else {
+        while (i < month) {
+            unix += daysInMonthArr[i]*secs_in_day;
+            i++;
+        }
+    }
+    unix += (day - 1)*secs_in_day;
+    return unix;
+}
+
+// ----------------------------------------------------------------------------
 // Parse string into [unix, day, month, year].
 // 'unix' is a number, the rest are strings.
 // If can't parse, return with no value.
@@ -9,6 +62,7 @@ function parseDate(str) {
     return [0, '1', 'January', '1970'];
 }
 
+// ----------------------------------------------------------------------------
 // HTTP request handling.
 app.get('/', function (request, response) {
     // For an empty URL, display an about page.
@@ -30,7 +84,10 @@ app.get('*', function (request, response) {
       // 'unix' is a number, the rest are strings.
       response.writeHead(200, { 'Content-Type' : 'application/json' });
       response.end(JSON.stringify(
-          { "unix": parsed[0], "natural": parsed[2] + " " + parsed[1] + ", " + parsed[3] }
+          { 
+            "unix": parsed[0], 
+            "natural": parsed[2] + " " + parsed[1] + ", " + parsed[3]
+          }
       ));
   }
 });
